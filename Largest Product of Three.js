09b422fd56ff1ae274947235341926edbc41542d
jackjhall22 <jackjhall22@me.com>
2016-09-28 08:26:53 -0500

    implement Largest Product of Three

---
+++ Largest Product of Three.js
@@ -0,0 +1,30 @@
+/*
+Largest Product of Three
+Write a function that accepts an array of integers and returns the largest product possible from three of those numbers.
+
+Example
+
+Input							Output
+
+array:
+[2,1,3,7]					42
+
+
+[0,2,3]						0
+*/
+
+///Solution///
+
+function largestProductOfThree (array) {
+var x = array.sort(function(a, b) {
+  return a - b;
+});
+var option1 = x[x.length-1]*x[x.length-2]*x[x.length-3];
+var option2 = x[0]*x[1]*x[x.length-1];
+  if (option1>option2){
+    return option1;
+  }
+  else{
+    return option2;
+  }
+}
