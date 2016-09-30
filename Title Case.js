749111b2fcdf43ed4cc00a03eb7a01d77e1686df
jackjhall22 <jackjhall22@me.com>
2016-09-30 11:42:07 -0500

    implement Title Case a Sentence

---
+++ Title Case.js
@@ -0,0 +1,24 @@
+/*
+Title Case a Sentence
+Return the provided string with the first letter of each word capitalized. 
+Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+Example
+
+Provided test cases
+titleCase(“I’m a little tea pot”) should return a string.
+titleCase(“I’m a little tea pot”) should return “I’m A Little Tea Pot”.
+titleCase(“sHoRt AnD sToUt”) should return “Short And Stout”.
+titleCase(“HERE IS MY HANDLE HERE IS MY SPOUT”) should return “Here Is My Handle Here Is My Spout”.
+
+*/
+
+///Solution///
+
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
