da9fa84a10e68e615362dda9f2ca25ea84e8786f
jackjhall22 <jackjhall22@me.com>
2016-10-16 13:46:29 -0500

    implement order

---
+++ String Order.js
@@ -0,0 +1,31 @@
+/*
+String Order
+
+Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+
+For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
+
+*/
+
+///Solution///
+
+function order(words){
+  let results = "";
+  let wordOrder = [];
+  words = words.split(' ');
+  words.forEach((word)=>{
+    for (var i = 0; i < word.length; i++){
+      if(!isNaN(parseInt(word[i]))){
+        wordOrder[word[i]-1] = word;
+      }
+    }
+  })
+  results = wordOrder.join(' ');
+  return results;
+}
+
+order('a1 b2 c3')
