fad20d42cf12479f615c8bd91e7dae1bc0363af5
jackjhall22 <jackjhall22@me.com>
2016-12-05 14:43:06 -0600

    implement split the bill

---
+++ Split The Bill.js
@@ -0,0 +1,50 @@
+/*
+It's tricky keeping track of who is owed what when spending money in a group. Write a function to balance the books.
+
+The function should take one parameter: an object/dict with two or more name-value pairs which represent the members of the group and the amount spent by each.
+The function should return an object/dict with the same names, showing how much money the members should pay or receive.
+Further points:
+
+The values should be positive numbers if the person should receive money from the group, negative numbers if they owe money to the group.
+If value is a decimal, round to two decimal places.
+Translations and comments (and upvotes!) welcome.
+
+Example
+
+3 friends go out together: A spends £20, B spends £15, and C spends £10. The function should return an object/dict showing that A should receive £5, B should receive £0, and C should pay £5.
+
+var group = {
+    A: 20, 
+    B: 15, 
+    C: 10
+}
+
+splitTheBill(group) // returns {A: 5, B: 0, C: -5}
+*/
+
+///Solution///
+
+function splitTheBill(x){
+var billTotal = 0;
+var splitEvenly = 0;
+var numberOfPeople = 0;
+var billObject = {};
+for (var key in x) {
+    numberOfPeople++;
+    billObject[key] = 0;
+    billTotal += x[key];
+}
+for (var key in x) {
+    splitEvenly = billTotal/numberOfPeople
+    if(x[key]=== splitEvenly){
+        continue;
+    }
+    if(x[key] > splitEvenly){
+        billObject[key] = Math.round((x[key] - splitEvenly) * 100) / 100;
+    } else {
+        billObject[key] = Math.round((x[key] - splitEvenly) * 100) / 100;  
+    }
+}
+
+return billObject;
+};
