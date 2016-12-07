7f61179be72dc7fe48f2535a9032596bd2fa91b6
jackjhall22 <jackjhall22@me.com>
2016-12-07 09:47:22 -0600

    implement charSum

---
+++ Character Sum.js
@@ -0,0 +1,30 @@
+/*
+Character Sum
+
+Given a string of arbitrary size, convert each character into its integer equivalent and sum the entirety.
+
+Examples
+
+Input										Output
+str:
+"123"										6
+str:
+"0101"									2
+str:
+"so cool!!1!"						1
+*/
+
+function charSum (str) {
+  var total = str.split('');
+  
+  total = total.map(function(num){
+	return Math.floor(num);
+    }).
+    filter(function(val){
+    	return isNaN(val)===false;
+    }).
+    reduce(function(sum, num){
+    	return sum + num;
+    });
+    return total;
+}
