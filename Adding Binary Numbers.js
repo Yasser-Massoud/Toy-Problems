1f4a101b3e985be4e8fcb1862a994feda09304cc
jackjhall22 <jackjhall22@me.com>
2017-01-10 10:58:13 -0600

    implement add

---
+++ Adding Binary Numbers.js
@@ -0,0 +1,108 @@
+/*
+Description:
+
+Task:
+
+You have to write a function add which takes two binary numbers as strings and returns their sum as a string.
+
+Note:
+
+You are not allowed to convert binary to decimal & vice versa.
+The sum should contain No leading zeroes.
+*/
+
+///Solution///
+function add(a,b){
+  // Happy Coding ^_^
+  num1 = a.split("");
+  num2 = b.split("");
+  // сравниваем длины
+  while (num1.length != num2.length) {
+    if (num1.length > num2.length) num2.unshift("0");
+    else num1.unshift("0");
+  }
+  var temp = 0;
+  var res = [];
+  // складываем
+  for(var i = num1.length - 1; i >= 0; i--) {
+    var temp2 = Number(num1[i]) + Number(num2[i]) + Number(temp);
+    if (temp2 > 2) {
+      temp2 = 1;
+      temp = 1;
+    } else if (temp2 == 2) {
+      temp2 = 0;
+      temp = 1;
+    } else temp = 0;
+    res.unshift(temp2);
+
+  }
+  // добавляем если нужно разряд
+  if (temp == 1) res.unshift("1");
+  // удаляем впереди нули если есть
+  while (res[0] != 1 && res.length > 1) {
+    res.shift();
+  }
+  return res.join("");
+};
+Best Practices0Clever0
+0ForkLink
+RogueAI
+
+function add(a,b){
+  a = trim0(a)
+  b = trim0(b)
+  
+  if( b.length > a.length) {
+    tmp = a
+    a = b
+    b = tmp
+  }
+  
+  result = ''
+  carry = false
+  
+  for(i = 1; i <= a.length; i++) {
+    if( a[a.length - i] === '1' && b[b.length - i] === '1' ) {
+      if( carry )
+        result = '1' + result
+      else
+        result = '0' + result
+      carry = true
+    }
+    else if( a[a.length - i] === '1' || b[b.length - i] === '1' ) {
+      if( carry ) {
+        result = '0' + result
+        carry = true
+      }
+      else {
+        result = '1' + result
+        carry = false
+      }
+    }
+    else {
+      if( carry )
+        result = '1' + result
+      else
+        result = '0' + result
+      carry = false
+    }
+  }
+  
+  if( carry ) 
+    result = '1' + result
+  
+  
+  return result
+}
+
+function trim0(str) {
+  if( str.length === 1) {
+    return str
+  }
+  
+  while( str[0] === '0') {
+    str = str.substr(1)
+  }
+  
+  return str
+}
