a392b667c514cdac57f117dd5ffc99bc4b58233e
Jack Hall <jackjhall22@me.com>
2017-08-20 12:01:18 -0700

    refactor

--- Buy a new car.js
+++ Buy a new car.js
@@ -30,18 +30,19 @@ nbMonths(8000, 8000, 1000, 1.5) should return [0, 0]
 
 /// Solutions ///
 
-function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
-  var months = 0, moneySaved = 0;
-  while (startPriceNew > startPriceOld + moneySaved){
-    moneySaved += savingperMonth;
-    startPriceOld -= (startPriceOld * (percentLossByMonth / 100));
-    startPriceNew -= (startPriceNew * (percentLossByMonth / 100));
+function nbMonths(startPriceOld, startPriceNew, savingsPerMonth, percentLossByMonth) {
+  let savings = 0
+  let months = 0
+  while (startPriceNew > startPriceOld + savings) {
+    savings += savingsPerMonth;
+    startPriceOld -= (startPriceOld * (percentLossByMonth/100));
+    startPriceNew -= (startPriceNew * (percentLossByMonth/100));
     months++;
-    if (months % 2 == 1){
-      percentLossByMonth += .5;
-    }
-  }
-  return [months, Math.round(startPriceOld + moneySaved - startPriceNew)];
+    months % 2 === 1 ? percentLossByMonth += 0.5 : null;
+    
+   }
+  
+  return [months, Math.round(startPriceOld + savings - startPriceNew)]
 }
 
 function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
