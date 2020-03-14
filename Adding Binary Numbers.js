9f71eb3cdc2a8d24d020709d9fa9917eac7e2cf3
jhal32 <jack.hall@nike.com>
2020-03-13 18:10:18 -0700

    update comments Adding Binary Numbers

--- Adding Binary Numbers.js
+++ Adding Binary Numbers.js
@@ -3,7 +3,7 @@ Description:
 
 Task:
 
-You have to write a function add which takes two binary numbers as strings and returns their sum as a string.
+Write a function `add` which takes two binary numbers as strings and returns their sum as a string.
 
 Note:
 
@@ -27,21 +27,20 @@ While there are still integers to add together from the binary numbers perform t
   else unshift the rest of the remaining integers in the longer binary number
 */
 
-
 ///Solution///
-function add(a,b){
+function add(a, b) {
   // Happy Coding ^_^
-  num1 = a.split("");
-  num2 = b.split("");
+  num1 = a.split('');
+  num2 = b.split('');
 
   while (num1.length != num2.length) {
-    if (num1.length > num2.length) num2.unshift("0");
-    else num1.unshift("0");
+    if (num1.length > num2.length) num2.unshift('0');
+    else num1.unshift('0');
   }
   var temp = 0;
   var res = [];
 
-  for(var i = num1.length - 1; i >= 0; i--) {
+  for (var i = num1.length - 1; i >= 0; i--) {
     var temp2 = Number(num1[i]) + Number(num2[i]) + Number(temp);
     if (temp2 > 2) {
       temp2 = 1;
@@ -51,72 +50,62 @@ function add(a,b){
       temp = 1;
     } else temp = 0;
     res.unshift(temp2);
-
   }
-  
-  if (temp == 1) res.unshift("1");
+
+  if (temp == 1) res.unshift('1');
 
   while (res[0] != 1 && res.length > 1) {
     res.shift();
   }
-  return res.join("");
-};
+  return res.join('');
+}
 
-function add(a,b){
-  a = trim0(a)
-  b = trim0(b)
-  
-  if( b.length > a.length) {
-    tmp = a
-    a = b
-    b = tmp
+function add(a, b) {
+  a = trim0(a);
+  b = trim0(b);
+
+  if (b.length > a.length) {
+    tmp = a;
+    a = b;
+    b = tmp;
   }
-  
-  result = ''
-  carry = false
-  
-  for(i = 1; i <= a.length; i++) {
-    if( a[a.length - i] === '1' && b[b.length - i] === '1' ) {
-      if( carry )
-        result = '1' + result
-      else
-        result = '0' + result
-      carry = true
-    }
-    else if( a[a.length - i] === '1' || b[b.length - i] === '1' ) {
-      if( carry ) {
-        result = '0' + result
-        carry = true
+
+  result = '';
+  carry = false;
+
+  for (i = 1; i <= a.length; i++) {
+    if (a[a.length - i] === '1' && b[b.length - i] === '1') {
+      if (carry) result = '1' + result;
+      else result = '0' + result;
+      carry = true;
+    } else if (a[a.length - i] === '1' || b[b.length - i] === '1') {
+      if (carry) {
+        result = '0' + result;
+        carry = true;
+      } else {
+        result = '1' + result;
+        carry = false;
       }
-      else {
-        result = '1' + result
-        carry = false
-      }
-    }
-    else {
-      if( carry )
-        result = '1' + result
-      else
-        result = '0' + result
-      carry = false
+    } else {
+      if (carry) result = '1' + result;
+      else result = '0' + result;
+      carry = false;
     }
   }
-  
-  if( carry ) 
-    result = '1' + result
-  
-  
-  return result
+
+  if (carry) result = '1' + result;
+
+  return result;
 }
 
 function trim0(str) {
-  if( str.length === 1) {
-    return str
+  if (str.length === 1) {
+    return str;
   }
-  
-  while( str[0] === '0') {
-    str = str.substr(1)
+
+  while (str[0] === '0') {
+    str = str.substr(1);
   }
-  
-  return str
+
+  return str;
 }
