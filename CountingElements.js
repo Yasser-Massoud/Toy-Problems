cef62c7f97d70faf477f4e92470c3c7e654080b8
jhal32 <jack.hall@nike.com>
2020-04-07 10:41:08 -0700

    start counting elements toy problem

---
+++ CountingElements.js
@@ -0,0 +1,20 @@
+/*
+  Give an integer array [arr], count element x such that x+1 is also in [arr]
+  If there's duplicates in [arr], count them seperately
+
+  Example 1: 
+  input: [1,2,3]
+  output: 2
+  Explanation: 1 and 2 are counted cause 2 and 3 are in the input.
+  Constraints: 
+    * 1 <= arr.length <=1000
+    * 0 <= arr[i] <= 1000
+*/
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const countElements = function(arr) {
+    
+};
