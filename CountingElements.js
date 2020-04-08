aa1278ad55ea0a125d6630be2aaad6d7ec72c9bb
jhal32 <jack.hall@nike.com>
2020-04-07 17:02:24 -0700

    Counting Elements from Leet code's thirty day challenge

--- CountingElements.js
+++ CountingElements.js
@@ -10,11 +10,31 @@
     * 1 <= arr.length <=1000
     * 0 <= arr[i] <= 1000
 */
-
+const input = [1,2,3]
 /**
  * @param {number[]} arr
  * @return {number}
  */
 const countElements = function(arr) {
-    
+  if(arr.length === 0 || arr.length > 1000) {
+    return 0;
+  }
+  let output = 0;
+  const hashset = arr.reduce((a, c) => {
+    if(c < 0 || c > 1000) return 0
+    let element = c + 1;
+    if(!a.hasOwnProperty(element)){
+      a[element] = 1;
+    } else {
+      a[element] += 1;
+    }
+    return a;
+  },{});
+  const hashSetKeys = Object.keys(hashset)
+  hashSetKeys.forEach(int => {
+    if(arr.includes(Number(int))) {
+      output += hashset[int] 
+    }
+  })
+  return output;
 };
