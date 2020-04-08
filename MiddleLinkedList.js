af970ab843f730f4b68c3dfc908a7972e20247c5
jhal32 <jack.hall@nike.com>
2020-04-08 09:16:44 -0700

    initial

---
+++ MiddleLinkedList.js
@@ -0,0 +1,28 @@
+/*
+  Given a non-empty, singly linked list with head node HEAD, return a middle node of linked list.
+  If there are two middle nodes, return the second middle node.
+
+  Example 1:
+  Input: [1,2,3,4,5]
+  Output: Node 3 form this list (Serialization: [3,4,5])
+  The returned node has a value 3. (The judge's serialization of this node is [3,4,5]).
+  Note that we returned a ListNode object ans, such that: 
+  ans.val = 3, ans,next.val =4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+
+  Note The number of nodes in the given list will be between 1 and 100
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = function(head) {
+    
+};
