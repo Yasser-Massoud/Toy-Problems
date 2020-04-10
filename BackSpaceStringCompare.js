ae375f966a49c09bd33b0a9429b4f79448d4fc4b
jhal32 <jack.hall@nike.com>
2020-04-10 12:03:15 -0600

    possible solutions

--- BackSpaceStringCompare.js
+++ BackSpaceStringCompare.js
@@ -1,3 +1,4 @@
+/*
 Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
 
 Input: S = "ab#c", T = "ad#c"
@@ -18,6 +19,9 @@ Note
 S and T only contain lowercase letters and '#' characters.
 
 Can you solve it in O(N) time and O(1) space?
+*/
+const S = "ab#c";
+const T = "ad#c";
 
 /**
  * @param {string} S
@@ -25,5 +29,32 @@ Can you solve it in O(N) time and O(1) space?
  * @return {boolean}
  */
 var backspaceCompare = function(S, T) {
-    
+  let currS, currT;
+  let typedS = '';
+  let typedT = '';
+  for (let i = 0; i < S.length; i++) {
+    currS = S[i];
+    if (currS !== "#") {
+      typedS += currS; 
+    }
+    if (currS === "#" && typedS.length) {
+      typedS = typedS.slice(0, typedS.length-1)
+    }
+  }
+  for (let i = 0; i < T.length; i++) {
+    currT = T[i];
+    prevCharT = i - 1 ;
+    if (currT !== "#") {
+      typedT += currT; 
+    } 
+    if (currT === "#" && typedT.length) {
+      prevCharT//?
+      typedT//?
+      typedT = typedT.slice(0, typedT.length-1)//?
+      typedT//
+    }
+  }
+  typedT//?
+  return typedS === typedT;  
 };
+backspaceCompare(S, T);//?
--- MinStack.js
+++ MinStack.js
@@ -1,3 +1,4 @@
+/*
 Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
 push(x) -- Push element x onto stack.
@@ -15,43 +16,77 @@ minStack.top();      --> Returns 0.
 minStack.getMin();   --> Returns -2.
 
 Consider each node in the stack having a minimum value. (Credits to @aakarshmadhavan)
-
+*/
 /**
  * initialize your data structure here.
  */
 var MinStack = function() {
-    
+    let storage = [];
+    let idx = 0
+    let min = null;
+    this.push = int => {
+      if (storage.length === 0) {
+        min = int;
+        storage.push({next: null, value: int, idx: 0})
+      } else {
+        storage.push( {next: storage[idx], value: int, idx})
+        idx += 1;
+        min = int > min ? min : int;
+      }
+    }
+    this.pop = () => {
+      if(storage.length === 0 ) return;
+      if(storage[storage.length-1].value === min) {
+        min = storage[0].value
+        storage.pop();
+        storage.forEach(obj => {
+          min = min < obj.value ? min : obj.value
+        })
+      } else {
+        storage.pop()
+      }
+    }
+    this.top = () => storage[storage.length-1].value
+    this.getMin = () => min;
+    this.storage = () => storage;
 };
 
-/** 
- * @param {number} x
- * @return {void}
- */
-MinStack.prototype.push = function(x) {
+// /** 
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MinStack.prototype.push = function(x) {
     
-};
+// };
 
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
+// /**
+//  * @return {void}
+//  */
+// MinStack.prototype.pop = function() {
     
-};
+// };
 
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.top = function() {
     
-};
+// };
 
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.getMin = function() {
     
-};
-
+// };
+var  minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   //?
+minStack.pop();
+minStack.top();      //?
+minStack.getMin();   //?
 /** 
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
